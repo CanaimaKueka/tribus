1f7a131c8eca41a667c9c35f380a216debf8009d
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2012-03-08 17:37:07 -0430

    ssss

--- settings.py
+++ settings.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os.path
+import os
 import glob
-
+#os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
 conffiles = glob.glob(os.path.join(os.path.dirname(__file__), 'setup', '*.py'))
 conffiles.sort()
 
 for f in conffiles:
+    print f
     execfile(os.path.abspath(f))
