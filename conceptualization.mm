e719b4f35ad0eae081964b85e54175708e043706
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2013-05-23 16:09:24 -0430

    Agregando mapa mental de conceptualización inicial.

---
+++ conceptualization.mm
@@ -0,0 +1,169 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1369324831794" ID="ID_856997049" MODIFIED="1369325106274" TEXT="Tribus">
+<node CREATED="1369325026725" ID="ID_1955727693" MODIFIED="1369340130910" POSITION="right" TEXT="Perfil de paquetes">
+<node CREATED="1369325329670" HGAP="21" ID="ID_1216443917" MODIFIED="1369338608382" TEXT="necesita " VSHIFT="-36">
+<node CREATED="1369325380896" HGAP="19" ID="ID_1935250742" MODIFIED="1369340828124" TEXT="procesos en segundo plano" VGAP="0" VSHIFT="35">
+<node CREATED="1369325401677" HGAP="25" ID="ID_864180024" MODIFIED="1369340259995" STYLE="fork" TEXT="recolectar informaci&#xf3;n del repositorio" VSHIFT="-26">
+<node CREATED="1369325913821" HGAP="25" ID="ID_1604525462" LINK="http://paquetes.canaima.softwarelibre.gob.ve/dists/kerepakupai/Release" MODIFIED="1369340287804" STYLE="fork" TEXT="${REPO}/dists/${DIST}/Release" VGAP="0" VSHIFT="-12">
+<node CREATED="1369336407900" HGAP="32" ID="ID_734449967" MODIFIED="1369338833346" TEXT="extraemos arquitecturas y componentes" VSHIFT="-34">
+<node CREATED="1369337168332" HGAP="27" ID="ID_1528113474" MODIFIED="1369338766118" TEXT="para listar" VSHIFT="-31">
+<node CREATED="1369325913821" HGAP="27" ID="ID_1559544295" LINK="http://paquetes.canaima.softwarelibre.gob.ve/dists/kerepakupai/main/binary-i386/Packages" MODIFIED="1369340308405" STYLE="fork" TEXT="${REPO}/dists/${DIST}/${COMP}/binary-${ARCH}/Packages" VGAP="0" VSHIFT="-36">
+<node CREATED="1369337805817" ID="ID_1383218726" MODIFIED="1369338012888" TEXT="Package"/>
+<node CREATED="1369337958279" ID="ID_512004224" MODIFIED="1369338010442" TEXT="Version"/>
+<node CREATED="1369337963689" ID="ID_501039877" MODIFIED="1369338007247" TEXT="Installed-Size"/>
+<node CREATED="1369338007247" ID="ID_1500177512" MODIFIED="1369338017388" TEXT="Maintainer"/>
+<node CREATED="1369338017389" ID="ID_275442078" MODIFIED="1369338041017">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Architecture
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338041021" ID="ID_1630127722" MODIFIED="1369338045437">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Depends
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338045441" ID="ID_509124833" MODIFIED="1369338049775">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pre-Depends
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338049779" ID="ID_1627315126" MODIFIED="1369338053483">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Size
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338053487" ID="ID_1559336939" MODIFIED="1369338056667">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Description
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338056671" ID="ID_1431171372" MODIFIED="1369338059811">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Homepage
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338059815" ID="ID_160987517" MODIFIED="1369338063091">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Section
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338063096" ID="ID_595563076" MODIFIED="1369338066590">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Priority
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1369338066594" ID="ID_66372843" MODIFIED="1369338066598">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Filename
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1369340215082" HGAP="43" ID="ID_865742097" MODIFIED="1369340671057" TEXT="clonar y actualizar VCS" VSHIFT="-32">
+<node CREATED="1369340283390" ID="ID_560035066" MODIFIED="1369340283390" TEXT=""/>
+</node>
+<node CREATED="1369326191987" HGAP="43" ID="ID_1813138061" MODIFIED="1369339395856" TEXT="alimentar la base de datos" VSHIFT="-92">
+<node CREATED="1369338496845" ID="ID_1827160714" MODIFIED="1369338496845" TEXT=""/>
+</node>
+</node>
+<node CREATED="1369340785235" HGAP="32" ID="ID_773857770" MODIFIED="1369340821794" TEXT="interfaz gr&#xe1;fica" VSHIFT="-45">
+<node CREATED="1369338610214" HGAP="19" ID="ID_528730895" MODIFIED="1369340819046" TEXT="que muestre" VSHIFT="-24">
+<node CREATED="1369339350995" ID="ID_1836743767" MODIFIED="1369339360569" TEXT="Informaci&#xf3;n b&#xe1;sica"/>
+<node CREATED="1369339075892" ID="ID_182001457" MODIFIED="1369339839778" TEXT="C&#xf3;digo fuente">
+<node CREATED="1369339845876" ID="ID_538951347" MODIFIED="1369339875836" TEXT="soporte para">
+<node CREATED="1369339878195" ID="ID_286044309" MODIFIED="1369339882630" TEXT="git"/>
+<node CREATED="1369339883597" ID="ID_1602709680" MODIFIED="1369339887877" TEXT="Bazaar"/>
+<node CREATED="1369339888582" ID="ID_1077194968" MODIFIED="1369339892121" TEXT="Mercurial"/>
+<node CREATED="1369339892751" ID="ID_1634833505" MODIFIED="1369339902001" TEXT="SVN"/>
+<node CREATED="1369339902898" ID="ID_1293381064" MODIFIED="1369339918420" TEXT="tarballs"/>
+</node>
+<node CREATED="1369339505592" ID="ID_205944265" MODIFIED="1369339542974" TEXT="debe listar por ramas y tags"/>
+<node CREATED="1369339729584" ID="ID_529336898" MODIFIED="1369339748826" TEXT="debe permitir editar los archivos y adjuntar parches"/>
+</node>
+<node CREATED="1369339094067" ID="ID_310350506" MODIFIED="1369339124485" TEXT="Traducciones">
+<node CREATED="1369339803035" HGAP="27" ID="ID_27714245" MODIFIED="1369340497738" TEXT="debe listar todos" VSHIFT="-24"/>
+</node>
+<node CREATED="1369339125349" ID="ID_1469775174" MODIFIED="1369339323458" TEXT="Documentaci&#xf3;n">
+<node CREATED="1369339807036" ID="ID_195543815" MODIFIED="1369339807036" TEXT=""/>
+</node>
+<node CREATED="1369339324133" ID="ID_752751251" MODIFIED="1369339331734" TEXT="Bugs">
+<node CREATED="1369339809408" ID="ID_526018080" MODIFIED="1369339809408" TEXT=""/>
+</node>
+<node CREATED="1369340772751" ID="ID_921047203" MODIFIED="1369340777236" TEXT="x"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1369340131866" ID="ID_1558234860" MODIFIED="1369340137530" POSITION="right" TEXT="Subir paquetes"/>
+<node CREATED="1369325052647" ID="ID_897220356" MODIFIED="1369325059238" POSITION="left" TEXT="Perfiles de usuario"/>
+<node CREATED="1369325061033" ID="ID_1147951553" MODIFIED="1369325100773" POSITION="right" TEXT="Sistema de valoraci&#xf3;n"/>
+<node CREATED="1369325107716" ID="ID_71251870" MODIFIED="1369325171287" POSITION="left" TEXT="Creaci&#xf3;n de sabores"/>
+</node>
+</map>
