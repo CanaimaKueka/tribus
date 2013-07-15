3dd2af06811e5b79d126f8699ab2685613c477ae
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2013-07-15 14:49:39 -0430

    Removiendo encabezado de Canaima Semilla.

--- tribus/config/gtk.py
+++ tribus/config/gtk.py
@@ -1,32 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# ==============================================================================
-# PAQUETE: canaima-semilla
-# ARCHIVO: scripts/c-s.sh
-# DESCRIPCIÓN: Script principal. Se encarga de invocar a los demás módulos y
-#              funciones según los parámetros proporcionados.
-# USO: ./c-s.sh [MÓDULO] [PARÁMETROS] [...]
-# COPYRIGHT:
-#       (C) 2010-2012 Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
-#       (C) 2012 Niv Sardi <xaiki@debian.org>
-# LICENCIA: GPL-3
-# ==============================================================================
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# COPYING file for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# CODE IS POETRY
+
 
 import os
 
