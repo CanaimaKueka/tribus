8e51eac97d33e11cccf5520b7602eed2f6a91cf2
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2013-08-15 09:31:56 -0430

    Agregando secret key al versionamiento para facilitar reproducción del ambiente de desarrollo (mientras sólo se use para propósitos de prueba y desarrollo, está bien).

--- tribus/config/web.py
+++ tribus/config/web.py
@@ -16,7 +16,6 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = ()
 MANAGERS = ADMINS
-SECRET_KEY = ''
 
 USE_I18N = True
 USE_L10N = True
@@ -57,7 +56,12 @@ AUTHENTICATION_BACKENDS = (
     'django_auth_ldap.backend.LDAPBackend',
 )
 
-SOCIAL_AUTH_ENABLED_BACKENDS = ()
+# This should be secret, but as we are in development, doesn't matter
+# Production settings should be set in tribus/config/web_production.py
+# Other local configuration should be set in tribus/config/web_local.py
+SECRET_KEY = 'oue0893ro5c^82!zke^ypu16v0u&%s($lnegf^7-vcgc^$e&$f'
+
+SOCIAL_AUTH_ENABLED_BACKENDS = ('twitter', 'facebook', 'google', 'github')
 SOCIAL_AUTH_DEFAULT_USERNAME = 'tribus'
 SOCIAL_AUTH_UID_LENGTH = 32
 SOCIAL_AUTH_ASSOCIATION_HANDLE_LENGTH = 32
@@ -65,15 +69,15 @@ SOCIAL_AUTH_NONCE_SERVER_URL_LENGTH = 32
 SOCIAL_AUTH_ASSOCIATION_SERVER_URL_LENGTH = 32
 SOCIAL_AUTH_ASSOCIATION_HANDLE_LENGTH = 32
 
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
-GOOGLE_OAUTH2_CLIENT_ID = ''
-GOOGLE_OAUTH2_CLIENT_SECRET = ''
-FACEBOOK_APP_ID=''
-FACEBOOK_API_SECRET=''
-FACEBOOK_EXTENDED_PERMISSIONS = []
-GITHUB_APP_ID = ''
-GITHUB_API_SECRET = ''
+TWITTER_CONSUMER_KEY = '1uxQKRiKzHYUl3QbQSQ'
+TWITTER_CONSUMER_SECRET = 'gLLJf5DIuJ4wvrVJI6cL553AIGdLjxnsUlwJbOKhw'
+GOOGLE_OAUTH2_CLIENT_ID = '241742098100.apps.googleusercontent.com'
+GOOGLE_OAUTH2_CLIENT_SECRET = 'AeJo0x0mS4SAtQF_TuAHsfGC'
+FACEBOOK_APP_ID='172639862908723'
+FACEBOOK_API_SECRET='ef4e623c629e9e5ca5632bdd703c80a4'
+FACEBOOK_EXTENDED_PERMISSIONS = ['email']
+GITHUB_APP_ID = 'c3d70354858107387ef8'
+GITHUB_API_SECRET = 'b9defd6193c11c8fb27c9f65ddaba0747524afcc'
 
 # Baseline configuration.
 AUTH_LDAP_SERVER_URI = "ldap://localhost"
@@ -246,8 +250,12 @@ try:
 except:
     pass
 
-
 try:
     from tribus.config.web_local import *
+except:
+    pass
+
+try:
+    from tribus.config.web_production import *
 except:
     pass
