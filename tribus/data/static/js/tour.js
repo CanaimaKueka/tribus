3e1e388a478010b32dde925d0a8694d1868b9010
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2013-09-06 11:20:13 -0430

    Estandarizando los nombres de las fuentes opensans. Mejorando header para usuarios autenticados y no autenticados. Iniciando construcción de la ilustración de inicio.

--- tribus/data/static/js/tour.js
+++ tribus/data/static/js/tour.js
@@ -24,10 +24,22 @@ $(document).ready(function() {
 	$('#slide_2').css('width', function() {
 		return $(window).width()*0.5;
 	});
+
+	
+	$('#slide_3').css('width', function() {
+		return $(window).width()*0.5;
+	});
+
 	$('#slide_3').css('height', function() {
-		return $(window).height()*1.5;
+		return $(window).height();
 	});
 	$('#slide_4').css('height', function() {
+		return $(window).height();
+	});
+	$('#slide_5').css('height', function() {
+		return $(window).height();
+	});
+	$('#slide_6').css('height', function() {
 		return $(window).height();
 	});
 	$('.scrollblock').css('width', function() {
@@ -47,8 +59,12 @@ $(document).ready(function() {
 		.animate('#slide_1b',{ delay: 3000, duration: 3000, property:'top', end:$(window).height()*(-1), pin: true})
 		.animate('#slide_2',{ delay: 0, duration: 1000, property:'top', start:-500, end:500})
 		.animate('#slide_2',{ delay: 0, duration: 1000, property:'left', start:0, end:$(window).width()*0.5})
-		.animate('#slide_3',{ delay: 0, duration: 3000, property:'top', start:200, end:600})
-		.animate('#slide_3',{ delay: 0, duration: 3000, property:'left', start:$(window).width()*0.5, end:300})
+		.animate('#slide_3',{ delay: 0, duration: 1000, property:'top', start:200, end:600})
+		.animate('#slide_3',{ delay: 0, duration: 1000, property:'left', start:$(window).width(), end:$(window).width()*0.5})		.animate('#slide_3',{ delay: 0, duration: 1000, property:'top', start:200, end:600})
+		.animate('#slide_4',{ delay: 300, duration: 2000, property:'top', start:300, end:600})
+		.animate('#slide_5',{ delay: 500, duration: 2000, property:'top', start:-200, end:700})
+		.animate('#slide_6',{ delay: 700, duration: 2000, property:'top', start:-600, end:800})
+		// .animate('#slide_4',{ delay: 0, duration: 1000, property:'left', start:$(window).width(), end:$(window).width()*0.5})
 		// .animate('#slide_4',{ delay: 2000, duration: 1000, property:'top', start:0, end:1500})
 
 });
