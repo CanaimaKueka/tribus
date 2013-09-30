f004b4b183de979ad19f0a4ee46e95167c5d8e45
Homero Hernandez <hsh283@gmail.com>
2013-09-30 15:46:34 -0430

    edicion de datos de usuario en perfil cambio de password en perfil modificacion de interfaz

--- tribus/web/profile/views.py
+++ tribus/web/profile/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from tribus.web.profile.forms import data_change
+
+
 
 
 
@@ -20,8 +23,22 @@ def EditUserProfile(request):
 
 
     if request.user.is_authenticated():
-        
-        return render_to_response('profile/edit.html', data, context)
+        if request.method == "POST":
+            print (dir (request.POST))
+            u = User.objects.get(username__exact = request.user.username)
+            print ("esto es la descripcion:",u.description)
+            u.description = request.POST['descripcion']
+            u.location = request.POST['location']
+            u.save()
+
+            return HttpResponseRedirect('/profile')
+            #return render_to_response('profile/profiles.html', data, context)
+
+        else:
+            form = data_change()
+            data['editForm'] = form
+            return render_to_response('profile/edit.html', data, context)
+
 
     return HttpResponseRedirect('/')
 
