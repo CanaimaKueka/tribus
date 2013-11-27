8bcb560af77e62dfc650f4ad5f85582f21c9d2c2
José Francisco Guerrero <jsfrncscg@gmail.com>
2013-11-26 20:15:17 -0430

    - Corrigiendo error en registro de paquetes. - Corrigiendo error en carga de modulos de angular. - Agregando algunas traducciones.

---
+++ tribus/data/static/js/full/search.angular.js
@@ -0,0 +1,6 @@
+// Declare use of strict javascript
+'use strict';
+
+// Application -----------------------------------------------------------------
+
+var tribus = angular.module('tribus',['Search']);
