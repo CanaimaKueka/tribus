ed47ef3e4e108f1dda570e3b5ac6b4dd2dd7769c
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2013-12-16 00:40:52 -0430

    PEP8 Clean code

--- tribus/common/commands.py
+++ tribus/common/commands.py
@@ -6,6 +6,7 @@ from tribus.common.logger import get_logger
 
 log = get_logger()
 
+
 class Helper(object):
 
     def __init__(self, *args, **kwargs):
@@ -22,14 +23,15 @@ class Helper(object):
 
 class Command(object):
     """
-    Wraps a shell command, so we don't have to store any kind of command line options in 
-    one of the git-buildpackage commands
+    Wraps a shell command, so we don't have to store any kind of command
+    line options in one of the git-buildpackage commands
     """
 
     def __init__(self, cmd, args=[], shell=False, extra_env=None, cwd=None):
         self.cmd = cmd
         self.args = args
-        self.run_error = "Couldn't run '%s'" % (" ".join([self.cmd] + self.args))
+        self.run_error = ("Couldn't run '%s'" %
+                         (" ".join([self.cmd] + self.args)))
         self.shell = shell
         self.retcode = 1
         self.cwd = cwd
@@ -40,14 +42,17 @@ class Command(object):
             self.env = None
 
     def __call(self, args):
-        """wraps subprocess.call so we can be verbose and fix python's SIGPIPE handling"""
+        """wraps subprocess.call so we can be verbose and fix
+           python's SIGPIPE handling"""
         def default_sigpipe():
             "restore default signal handler (http://bugs.python.org/issue1652)"
             signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
         log.debug("%s %s %s" % (self.cmd, self.args, args))
-        cmd = [ self.cmd ] + self.args + args
-        if self.shell: # subprocess.call only cares about the first argument if shell=True
+        cmd = [self.cmd] + self.args + args
+        # subprocess.call only cares about
+        # the first argument if shell=True
+        if self.shell:
             cmd = " ".join(cmd)
         return subprocess.call(cmd, cwd=self.cwd, shell=self.shell,
                                env=self.env, preexec_fn=default_sigpipe)
@@ -62,7 +67,8 @@ class Command(object):
         try:
             retcode = self.__call(args)
             if retcode < 0:
-                err_detail = "%s was terminated by signal %d" % (self.cmd, -retcode)
+                err_detail = ("%s was terminated by signal %d" %
+                             (self.cmd, -retcode))
             elif retcode > 0:
                 err_detail = "%s returned %d" % (self.cmd, retcode)
         except OSError as e:
@@ -87,7 +93,8 @@ class Command(object):
             raise CommandExecFailed
 
     def call(self, args):
-        """like __call__ but don't use stderr and let the caller handle the return status
+        """like __call__ but don't use stderr and let the caller
+        handle the return status
         >>> Command("/bin/true").call(["foo", "bar"])
         0
         >>> Command("/foo/bar").call(["foo", "bar"]) # doctest:+ELLIPSIS
@@ -98,5 +105,5 @@ class Command(object):
         try:
             ret = self.__call(args)
         except OSError, e:
-            raise CommandExecFailed, "Execution failed: %s" % e
+            raise CommandExecFailed("Execution failed: %s" % e)
         return ret
