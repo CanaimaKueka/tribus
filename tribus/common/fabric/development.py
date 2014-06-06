701bab796042bfdfef4cfbde3404ac24a46d35d1
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2014-06-06 17:23:07 -0430

    Mejorando Dockerfile.

--- tribus/common/fabric/development.py
+++ tribus/common/fabric/development.py
@@ -25,7 +25,7 @@ from tribus.config.ldap import (AUTH_LDAP_SERVER_URI, AUTH_LDAP_BASE,
                                 AUTH_LDAP_BIND_DN, AUTH_LDAP_BIND_PASSWORD)
 from tribus.config.pkg import (debian_dependencies, python_dependencies,
                                preseed_db, preseed_debconf, preseed_env,
-                               preseed_ldap, mount_volumes)
+                               preseed_ldap)
 from tribus.common.logger import get_logger
 from tribus.common.utils import get_path
 from tribus.common.system import get_local_arch
@@ -64,17 +64,21 @@ def development():
     env.ldap_writer = AUTH_LDAP_BIND_DN
     env.ldap_server = AUTH_LDAP_SERVER_URI
 
+    mount_volumes = ['/var/cache/apt-cacher-ng:/var/cache/apt-cacher-ng:rw',
+                     '/var/cache/apt:/var/cache/apt:rw',
+                     '%(basedir)s:%(docker_basedir)s:rw']
     env.mount_volumes = '\n'.join('VOLUME %s' % i
                                   for i in mount_volumes)
-    env.mount = ['/var/cache/apt-cacher-ng:/var/cache/apt-cacher-ng:rw',
-                 '/var/cache/apt:/var/cache/apt:rw',
-                 '%(basedir)s:%(docker_basedir)s:rw']
 
 
 def local_configure_sudo():
     run(('su root -c '
          '"echo \'%(user)s ALL= NOPASSWD: ALL\' '
-         '> /etc/sudoers.d/tribus"') % env, capture=False)
+         '> /etc/sudoers.d/tmp"') % env, capture=False)
+
+
+def local_deconfigure_sudo():
+    run(('su root -c "rm -rf /etc/sudoers.d/tmp"') % env, capture=False)
 
 
 def generate_dockerfile():
@@ -82,15 +86,19 @@ def generate_dockerfile():
          'FROM %(docker_base_image)s\n'
          'MAINTAINER Luis A. Martínez F. <luis@huntingbears.com.ve>\n'
          '%(docker_preseed_env)s\n'
-         'VOLUME []'
-         'WORKDIR %(docker_basedir)s'
+         '%(mount_volumes)s\n'
+         'WORKDIR %(docker_basedir)s\n'
          'RUN echo $\'%(preseed_debconf)s\' | debconf-set-selections\n'
          'RUN apt-get update\n'
          'RUN apt-get install %(apt_args)s %(debian_dependencies)s\n'
          'RUN echo "postgres:tribus" | chpasswd\n'
          'RUN sudo -i -u postgres /bin/sh -c "psql -c \'%(preseed_db)s\'"\n'
-         'RUN echo $\'%(preseed_ldap)s\' | ldapadd -x -w "%(ldap_passwd)s" -D "%(ldap_writer)s" -H "%(ldap_server)s"\n'
+         'RUN echo $\'%(preseed_ldap)s\' | '
+         'ldapadd -x -w "%(ldap_passwd)s" '
+         '-D "%(ldap_writer)s" -H "%(ldap_server)s"\n'
          'RUN pip install %(python_dependencies)s\n'
+         'RUN python manage.py syncdb'
+         'RUN python manage.py migrate'
          '" > %(dockerfile_path)s') % env, capture=False)
 
 
